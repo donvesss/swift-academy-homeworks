bfeae84807d10b9383fc4772a2da76542c83bfcc
veselin ivanov <donvesss@abv.bg>
2016-04-17 16:06:30 +0300

    project

---
+++ FB/index.php
@@ -0,0 +1,15 @@
+<?php
+	
+	require_once "validate.php";
+	require_once "src/header.php";
+	require_once "src/mainmenu.php";
+?>
+	<h2>Welcome to Facebook!!!</h2>
+
+	<p>
+	Your can start to <a href="register.php">register</a> as a new user.
+	Then, you can <a href="search.php">search</a> others users.
+	And you can <a href="login.php">login</a> to access your private area.
+	</p>
+<?php	
+	require_once "src/footer.php";
